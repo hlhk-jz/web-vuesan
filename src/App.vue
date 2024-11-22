408e9b1e9630453e9b3c685447bfb1419b984da2
466972777@qq.com <466972777@qq.com>
2024-11-23 07:47:00 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ResponsApi/>
+    </div>
+</template>
+
+<script>
+import ResponsApi from "./components/view/ResponsApi"
+
+export default {
+  name: 'App',
+  components: {
+      ResponsApi
+  }
+}
+</script>
+
+
---
+++ src/components/view/ResponsApi.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>我是响应式API</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
